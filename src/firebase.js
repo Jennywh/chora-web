f3c9cbeba97a1e1f9b277d7e3250e8eb6722bc4b
Jennywh <jennyhan_wang@outlook.com>
2024-12-23 21:17:02 -0400

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0wha7eRN7VwUtJWkG2cpwhNlwiO48qfA',
-  authDomain: 'chores-ad8e8.firebaseapp.com',
-  databaseURL: 'https://chores-ad8e8-default-rtdb.firebaseio.com',
-  projectId: 'chores-ad8e8',
-  storageBucket: 'chores-ad8e8.firebasestorage.app',
-  messagingSenderId: '1028375070162',
-  appId: '1:1028375070162:web:3533ddd3dd5f779a48b40b',
-  measurementId: 'G-FX5ZZXQY65',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
